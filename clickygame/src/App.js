bbc7a2805eb063978cfd327f0182d8caab571ba0
Brandon James <bjames9076@gmail.com>
2018-02-12 21:02:13 -0500

    added more componets

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,21 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cats from './cats.json'
+import Wrapper from './components/Wrapper'
+import Navpills from './components/Navpills'
+import Title from './components/Title'
+import CatCard from './components/CatCard'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        message: "Click an image to begin!",
+        topScore: 0,
+        curScore: 0,
+        cats: cats,
+        unselectedCats: cats
+    }
+
+    componentDidMount() {
+    }
+
+    shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    selectCat = breed => {
+        const findCat = this.state.unselectedCats.find(item => item.breed === breed);
+
+        if(findCat === undefined) {
+            // failure to select a new cat
+            this.setState({ 
+                message: "You guessed incorrectly!",
+                topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+                curScore: 0,
+                cats: cats,
+                unselectedCats: cats
+            });
+        }
+        else {
+            // success to select a new cat
+            const newCats = this.state.unselectedCats.filter(item => item.breed !== breed);
+            
+            this.setState({ 
+                message: "You guessed correctly!",
+                curScore: this.state.curScore + 1,
+                cats: cats,
+                unselectedCats: newCats
+            });
+        }
+
+        this.shuffleArray(cats);
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Navpills
+                    message={this.state.message}
+                    curScore={this.state.curScore}
+                    topScore={this.state.topScore}
+                />
+                <Title />
+                {
+                    this.state.cats.map(cat => (
+                        <CatCard
+                            breed={cat.breed}
+                            image={cat.image}
+                            selectCat={this.selectCat} 
+                            curScore={this.state.curScore}
+                        />
+                    ))
+                }
+            </Wrapper>
+        );
+    }
 }
 
 export default App;
---
+++ clickygame/src/components/CatCard/CatCard.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import "./CatCard.css";
+
+const CatCard = props => (
+    <div className="card">
+        <div className="img-container">
+            <a onClick={() => props.selectCat(props.breed)} 
+                className={props.curScore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}
+            >
+                <img alt={props.breed} src={props.image} />
+            </a>
+        </div>
+    </div>
+);
+
+export default CatCard;
